78d113200f282945421ac2d7babac8005f6db2f1
Ian <s1104168@student.hsleiden.nl>
2017-12-14 13:17:05 +0100

    Uren inzien werkt

--- src/app/components/services/api.service.ts
+++ src/app/components/services/api.service.ts
@@ -5,13 +5,13 @@ import { Observable } from 'rxjs/Observable';
 import { AuthorizationService } from './authorization.service';
 import { error } from 'selenium-webdriver';
 
-import { User } from '../user/user'
+import { User } from '../user/user';
 import { URLSearchParams } from '@angular/http';
 
 @Injectable()
-export class ApiService{
+export class ApiService {
 
-    constructor(private http: HttpClient, private authService: AuthorizationService){
+    constructor(private http: HttpClient, private authService: AuthorizationService) {
 
     }
 
