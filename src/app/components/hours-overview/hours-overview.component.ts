7420c4dd923f14a71e70fe311a9511793a567153
Ian <s1104168@student.hsleiden.nl>
2018-01-08 13:09:19 +0100

    Mooier gemaakt

--- src/app/components/hours-overview/hours-overview.component.ts
+++ src/app/components/hours-overview/hours-overview.component.ts
@@ -31,7 +31,7 @@ export class HoursOverviewComponent implements OnInit {
     this.filteredHours = this.filterPipe.transform(this.hours, 'customerName', this.searchCustomer);
     this.filteredHours = this.filterPipe.transform(this.filteredHours, 'projectName', this.searchProject);
     this.filteredHours = this.filterPipe.transform(this.filteredHours, 'subjectName', this.searchSubject);
-    this.csvService.download(this.filteredHours, 'lol.csv');
+    this.csvService.download(this.filteredHours, 'Gewerkte Uren '+new Date().toDateString());
   }
 
   ngOnInit(){
