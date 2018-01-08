d89ce64f89bd37e0c633802605c3824cd6114ef2
Ian <s1104168@student.hsleiden.nl>
2018-01-08 12:18:10 +0100

    Knop toegevoegd

--- src/app/components/hours-overview/hours-overview.component.ts
+++ src/app/components/hours-overview/hours-overview.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Hours } from '../../models/hours';
 import { HoursService } from '../../services/hours.service';
-import { Observable } from 'rxjs/Observable';
+import { CsvService } from 'angular2-json2csv';
 
 @Component({
   selector: 'app-hours-overview',
@@ -10,17 +10,23 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HoursOverviewComponent implements OnInit {
   hours: Hours[];
+  filteredHours: Hours[];
+
   public searchStartDate: string;
   public searchEndDate: string;
   public searchCustomer: string;
   public searchProject: string;
   public searchSubject: string;
 
-  constructor(private hoursService: HoursService) {
+  constructor(private hoursService: HoursService, private csvService: CsvService) {
     this.hoursService.getAll().subscribe(hours => {
       this.hours = hours;
     });
-    
+
+  }
+
+  createCsv(){
+    this.csvService.download(this.hours, 'lol.csv');
   }
 
   ngOnInit(){
