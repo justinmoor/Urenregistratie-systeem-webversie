386d13850cee4ab481ebbe5040daa1f4a625577f
Justin <justin-804@hotmail.com>
2018-01-18 09:51:42 +0100

    Headers

--- src/app/models/subject.ts
+++ src/app/models/subject.ts
@@ -1,11 +1,7 @@
 export class Subject{
     constructor( 
         public subjectID?:number,
-<<<<<<< HEAD
-        public subjectName?:String,
-=======
         public subjectName?:string,
->>>>>>> 5d38c0c76cc415837e76ff562399906262891556
         public projectID?:number
         ) {
        
